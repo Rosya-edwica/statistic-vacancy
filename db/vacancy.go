9a3bdf10838f5740d2374266bdc6fa6ce4405d9e
Rosya-edwica <yunoshev.dev@gmail.com>
2023-11-02 14:00:13 +0300

    не учитываем вакансии из geekjob, так как там много вакансий с зарплатами 30млн рублей+

--- db/vacancy.go
+++ db/vacancy.go
@@ -6,8 +6,8 @@ import (
 	"github.com/Rosya-edwica/statistic-vacancy/models"
 )
 
-func (d *Database) GetVacancies(pos models.Position) (vacancies []models.Vacancy){
-	query := fmt.Sprintf("select id, experience, salary_from, salary_to, prof_areas, specs FROM h_vacancy WHERE position_id=%d AND city_id=%d", pos.PositionId, pos.CityId)
+func (d *Database) GetVacancies(pos models.Position) (vacancies []models.Vacancy) {
+	query := fmt.Sprintf("select id, experience, salary_from, salary_to, prof_areas, specs FROM h_vacancy WHERE position_id=%d AND city_id=%d AND platform != 'geekjob", pos.PositionId, pos.CityId)
 	rows, err := d.Connection.Query(query)
 	checkErr(err)
 	defer rows.Close()
@@ -17,13 +17,13 @@ func (d *Database) GetVacancies(pos models.Position) (vacancies []models.Vacancy
 		var salary_from, salary_to float64
 		err = rows.Scan(&id, &experience, &salary_from, &salary_to, &prof_areas, &specs)
 		vacancies = append(vacancies, models.Vacancy{
-			Id: id,
+			Id:         id,
 			Experience: experience,
 			SalaryFrom: salary_from,
-			SalaryTo: salary_to,
-			Area: prof_areas,
-			Spec: specs,
-			CityId: pos.CityId,
+			SalaryTo:   salary_to,
+			Area:       prof_areas,
+			Spec:       specs,
+			CityId:     pos.CityId,
 			PositionId: pos.PositionId,
 		})
 	}
